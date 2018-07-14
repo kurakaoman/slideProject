6c702f60d4d64ca8cf7451263d32be3a090e3f4f
kurakaoman <kurakaoman@gmail.com>
2018-07-14 18:18:48 +0200

    Aplikacja się  uruchamia
    zrobione Admin registration :-) działą
    po wprowadzeniu danych name i password w htmlu adminRegistration.html przesyla dane i tworzy nowego admina w bazie, a nastepnie przenosi nas do nowego htmla przesyaljac id tego admina , wyciaga z niego name i wypisuje na stronce ( juz na adminPanel.html )
    
    doszla klasa User ( bedzie mial wszystkie role)

---
+++ src/main/java/com/slide/project/demo/roles/user/User.java
@@ -0,0 +1,101 @@
+package com.slide.project.demo.roles.user;
+
+import com.slide.project.demo.roles.admin.Admin;
+import com.slide.project.demo.roles.customer.Customer;
+import com.slide.project.demo.roles.manager.Manager;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Collection;
+
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String username;
+    private String password;
+    private String phoneNumber;
+    private Admin admin;
+    private Customer customer;
+    private Manager manager;
+
+    public User(){
+
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Admin getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(Admin admin) {
+        this.admin = admin;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Manager getManager() {
+        return manager;
+    }
+
+    public void setManager(Manager manager) {
+        this.manager = manager;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
